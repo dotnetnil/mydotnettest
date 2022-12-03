0e3669dca279e8805bd00e3cc8fda1d5c4f54842
dotnetnil <nileshjob2019@gmail.com>
2022-12-03 23:15:21 +0530

    staged

--- gitWebApplication1/gitWebApplication1/Controllers/HomeController.cs
+++ gitWebApplication1/gitWebApplication1/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace gitWebApplication1.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page test.";
 
             return View();
         }
